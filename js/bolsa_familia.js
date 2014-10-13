4be85bf0b1ab819adfc52caf8e6ae343a79e933b
Rodrigo Burgarelli <rodrigo.burgarelli1@gmail.com>
2014-10-13 16:28:34 -0300

    com checkboxes

--- js/bolsa_familia.js
+++ js/bolsa_familia.js
@@ -1,8 +1,7 @@
 var height = 600
-
-var id_regiao_selected = "Norte";
 var dados = null;
-var enable_br = true;
+var chart = null
+var selecionados = ["Norte"]
 
 function carregaDados() {
     jQuery.ajax("bolsa.csv", {
@@ -17,13 +16,6 @@ function carregaDados() {
         }
     });
         
-}
-
-function ativa_radio(regiao) {
-    var sreg = document.Regioes.sregiao;
-    for (var id = 0; id < sreg.length; id++) {
-        if (sreg[id].value == regiao) jQuery(sreg[id]).attr('disabled', false);
-    }
 }
 
 function arrumaTooltip(chart) {
@@ -56,7 +48,7 @@ function addCommas(nStr) {
 function graficoNorte() {
     var svg = dimple.newSvg("#bolsa", window.width, window.height);
     svg[0][0].setAttribute('id', 'svg_Norte');
-    var local_data = dimple.filterData(dados, "regiao", "Norte");
+    var local_data = dimple.filterData(dados, "regiao", selecionados);
     var myChart = new dimple.chart(svg, local_data),
         x = myChart.addMeasureAxis("x", "porc_bf"),
         y = myChart.addMeasureAxis("y", "dilma.vs.2014"),
@@ -68,132 +60,25 @@ function graficoNorte() {
     z.overrideMin = 1;
     y.title = '% de votos para Dilma'
     x.title = '% das familias que recebe Bolsa Familia'
-    myChart.addSeries(["mun", "uf"], dimple.plot.bubble);
+    myChart.addSeries(["mun", "uf","regiao"], dimple.plot.bubble);
     myChart = arrumaTooltip(myChart)
     myChart.addLegend(60, 10, 800, 60, "left");
     myChart.draw();
-    ativa_radio("Norte");
-}
-
-function graficoCentroOeste() {
-    var svg = dimple.newSvg("#bolsa", window.width, window.height);
-    svg[0][0].setAttribute('id', 'svg_Centro-Oeste');
-    var local_data = dimple.filterData(dados, "regiao", "Centro-Oeste");
-    var myChart = new dimple.chart(svg, local_data),
-        x = myChart.addMeasureAxis("x", "porc_bf"),
-        y = myChart.addMeasureAxis("y", "dilma.vs.2014"),
-        z = myChart.addMeasureAxis("z", "pop.2011");
-        x.overrideMax = 100;
-        y.overrideMax = 100;
-        z.overrideMax = 11316119;
-        z.overrideMin = 1;
-        y.title = '% de votos para Dilma'
-        x.title = '% das familias que recebe Bolsa Familia'
-        myChart.addSeries(["mun", "uf"], dimple.plot.bubble);
-        myChart = arrumaTooltip(myChart)
-    myChart.addLegend(60, 10, 800, 60, "left");
-    myChart.draw();
-    hideRegion("Centro-Oeste");
-    ativa_radio("Centro-Oeste");
-}
-
-function graficoSul() {
-    var svg = dimple.newSvg("#bolsa", window.width, window.height);
-    svg[0][0].setAttribute('id', 'svg_Sul');
-    var local_data = dimple.filterData(dados, "regiao", "Sul");
-    var myChart = new dimple.chart(svg, local_data),
-        x = myChart.addMeasureAxis("x", "porc_bf"),
-        y = myChart.addMeasureAxis("y", "dilma.vs.2014"),
-        z = myChart.addMeasureAxis("z", "pop.2011");
-        x.overrideMax = 100;
-        y.overrideMax = 100;
-        z.overrideMax = 11316119;
-        z.overrideMin = 1;
-        y.title = '% de votos para Dilma'
-        x.title = '% das familias que recebe Bolsa Familia'
-        myChart.addSeries(["mun", "uf"], dimple.plot.bubble);
-        myChart = arrumaTooltip(myChart)
-    myChart.addLegend(60, 10, 800, 60, "left");
-    myChart.draw();
-    hideRegion("Sul");
-    ativa_radio("Sul");
-}
-
-function graficoNordeste() {
-    var svg = dimple.newSvg("#bolsa", window.width, window.height);
-    svg[0][0].setAttribute('id', 'svg_Nordeste');
-    var local_data = dimple.filterData(dados, "regiao", "Nordeste");
-    var myChart = new dimple.chart(svg, local_data),
-        x = myChart.addMeasureAxis("x", "porc_bf"),
-        y = myChart.addMeasureAxis("y", "dilma.vs.2014"),
-        z = myChart.addMeasureAxis("z", "pop.2011");
-        x.overrideMax = 100;
-        y.overrideMax = 100;
-        z.overrideMax = 11316119;
-        z.overrideMin = 1;
-        y.title = '% de votos para Dilma'
-        x.title = '% das familias que recebe Bolsa Familia'
-        myChart.addSeries(["mun", "uf"], dimple.plot.bubble);
-        myChart = arrumaTooltip(myChart)
-    myChart.addLegend(60, 5, 800, 80, "left");
-    myChart.draw();
-    hideRegion("Nordeste");
-    ativa_radio("Nordeste");
-}
-
-function graficoSudeste() {
-    var svg = dimple.newSvg("#bolsa", window.width, window.height);
-    svg[0][0].setAttribute('id', 'svg_Sudeste');
-    var local_data = dimple.filterData(dados, "regiao", "Sudeste");
-    var myChart = new dimple.chart(svg, local_data),
-        x = myChart.addMeasureAxis("x", "porc_bf"),
-        y = myChart.addMeasureAxis("y", "dilma.vs.2014"),
-        z = myChart.addMeasureAxis("z", "pop.2011");
-        x.overrideMax = 100;
-        y.overrideMax = 100;
-        z.overrideMax = 11316119;
-        z.overrideMin = 1;
-        y.title = '% de votos para Dilma'
-        x.title = '% das familias que recebe Bolsa Familia'
-        myChart.addSeries(["mun", "uf"], dimple.plot.bubble);
-        myChart = arrumaTooltip(myChart)
-    myChart.addLegend(60, 10, 800, 60, "left");
-    myChart.draw();
-    hideRegion("Sudeste");
-    ativa_radio("Sudeste");
-}
-
-function graficoBrasil() {
-    var svg = dimple.newSvg("#bolsa", window.width, window.height);
-    svg[0][0].setAttribute('id', 'svg_Brasil');
-    var myChart = new dimple.chart(svg, dados),
-        x = myChart.addMeasureAxis("x", "porc_bf"),
-        y = myChart.addMeasureAxis("y", "dilma.vs.2014"),
-        z = myChart.addMeasureAxis("z", "pop.2011");
-        x.overrideMax = 100;
-        y.overrideMax = 100;
-        z.overrideMax = 11316119;
-        z.overrideMin = 1;
-        y.title = '% de votos para Dilma'
-        x.title = '% das familias que recebe Bolsa Familia'
-        myChart.addSeries(["mun", "uf","regiao"], dimple.plot.bubble);
-        myChart = arrumaTooltip(myChart)
-    myChart.addLegend(60, 10, 800, 60, "left");
-    myChart.draw();
-    hideRegion("Brasil");
-    ativa_radio("Brasil");
-}
-
-function hideRegion(regiao) {
-    jQuery("#svg_" + regiao).hide();
+    window.chart = myChart
 }
 
 function showRegion(regiao) {
-    if (regiao == "Brasil" && enable_br) {
-        enable_br = false;
-        graficoBrasil();
+    data = window.dados
+    chart = window.chart
+    selecionados = window.selecionados
+    if (selecionados.indexOf(regiao) > -1) {
+        selecionados.splice(selecionados.indexOf(regiao),1)
+    } else {
+        selecionados.push(regiao)
     }
-    hideRegion(id_regiao_selected);
-    jQuery("#svg_" + regiao).show();
-    id_regiao_selected = regiao;
+    data = dimple.filterData(data,"regiao",selecionados)
+    chart.data = data
+    chart.draw()
+    window.chart = chart
+    window.selecionados = selecionados
 }
